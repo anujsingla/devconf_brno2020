45b23e8dc1d957388eb3f1873e8c05be2f8657f8
Anuj Singla <asingla@redhat.com>
2020-01-12 17:28:08 +0530

    code changes.

---
+++ src/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Alert} from 'react-bootstrap';
+
+export default function Footer() {
+    return (
+        <footer>
+            <Alert variant='primary'>
+                this is footer of the page.
+            </Alert>
+        </footer>
+    )
+}
