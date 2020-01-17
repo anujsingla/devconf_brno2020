78b80139e73fb801df2acc250e34f8cdce198c74
Anuj Singla <asingla@redhat.com>
2020-01-17 11:46:02 +0530

    tree-shaking

---
+++ src/mathUtility.ts
@@ -0,0 +1,9 @@
+export function square(x: number) {
+    console.log('square', x);
+    return x * x;
+}
+  
+export function cube(x: number) {
+    console.log('cube', x);
+    return x * x * x;
+}
